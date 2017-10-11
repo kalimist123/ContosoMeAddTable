8a288960aed62b3dc25b4f7af75e8b8afe3bd911
JackTorrence <jacktorrence420@gmx.com>
2017-10-11 18:25:51 +0100

    Create Tests

---
+++ ContosoUniversity.IntegrationTests/Features/SchoolBooks/CreateTests.cs
@@ -0,0 +1,44 @@
+﻿namespace ContosoUniversity.IntegrationTests.Features.SchoolBooks
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using ContosoUniversity.Features.SchoolBooks;
+    using Models;
+    using Shouldly;
+    using Xunit;
+    using static SliceFixture;
+
+    public class CreateTests : IntegrationTestBase
+    {
+        [Fact]
+        public async Task Should_create_new_schoolbook()
+        {
+            
+
+            Create.Command command = null;
+
+            await ExecuteDbContextAsync(async (db, mediator) =>
+            {
+                
+
+                command = new Create.Command
+                {
+                    Author ="JohnnyOnion",
+                    Name = "Engineering 5",
+                    Price=5
+                };
+
+                await mediator.Send(command);
+            });
+
+            var created = await ExecuteDbContextAsync(db => db.SchoolBooks.Where(d => d.Name == command.Name).SingleOrDefaultAsync());
+
+            created.ShouldNotBeNull();
+            created.Author.ShouldBe(command.Author);
+            created.Name.ShouldBe(command.Name);
+            created.Price.ShouldBe(command.Price.GetValueOrDefault());
+        }
+    }
+}
